2e7352214df5fa22bc7f3eee3144394c1b3e4064
NesjsjsjsjsjsA <00133723@uca.edu.sv>
2025-09-26 23:50:28 -0600

    Done

--- P4/script.js
+++ P4/script.js
@@ -1,1 +1,42 @@
-console.log("Te confirmo que funciona!")
+document.addEventListener("DOMContentLoaded", () => {
+  const textElM = document.querySelector("#secretWord");
+
+  function day() {
+    let dayW = prompt("dia de la semana :D").toLowerCase();
+    let caseR;
+
+    switch (dayW) {
+      case "lunes":
+        caseR = "atender un cliente especificos";
+        break;
+      case "martes":
+        caseR = "visitar una agencia fuera de la ciudad";
+        break;
+      case "miercoles":
+        caseR = "llevar a mi hija al ballet (balé)";
+        break;
+      case "jueves":
+        caseR = "priorizar entregas de desarrollo";
+        break;
+      case "viernes":
+        caseR = "atender problemas de manera remota";
+        break;
+      case "sabado":
+        caseR = "hacer lo que mi esposa quiera";
+        break;
+
+      default:
+        caseR = "O es domingo o no has puesto bien el día";
+    }
+
+    changeTitle(caseR);
+  }
+
+  function changeTitle(text) {
+    textElM.textContent = "Usted debe " + text;
+    textElM.style.color = "purple";
+    textElM.fontStyle = "italic";
+  }
+
+  document.querySelector("#action").onclick = day;
+});
